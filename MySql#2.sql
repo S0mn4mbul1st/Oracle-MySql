4c0794734b324af7cdbb19d738b8c02d95664b58
Aykhan Imanov <42781916+S0mn4mbul1st@users.noreply.github.com>
2020-10-22 18:21:17 +0200

    Add files via upload

---
+++ MySql#2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE aykhan_240745.uniform (
+    name VARCHAR(40),
+    size ENUM('x-small', 'small', 'medium', 'large', 'x-large')
+);
+INSERT INTO aykhan_240745.uniform (name, size) VALUES ('dress shirt','large'), ('shirt','medium'),
+  ('polo shirt','small');
